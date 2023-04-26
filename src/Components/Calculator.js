4743a44328cae156685e25373d07d4e1391681ba
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-04-26 18:36:36 -0500

    intall big.js library, add calculate.js and operate.js to manage the calculator logic, try some useState

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,15 +1,27 @@
-export default function Calculator() {
-  const buttons = ['AC', '+/-', '%', '/', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+import React, { useState } from 'react';
+//  import calculate from '../logic/calculate';
+
+const Calculator = () => {
+  const buttons = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
     <>
       <div className="calculator">
-        <p className="outputResult">0</p>
+        <p className="outputResult">{count}</p>
         <div className="buttonContainer">
           {buttons.map((button) => (
-            <button type="button" className="calcButtons" key={button}>{button}</button>
+            <button type="button" className="calcButtons" key={button} onClick={handleClick}>{button}</button>
           ))}
         </div>
       </div>
     </>
   );
-}
+};
+
+export default Calculator;
