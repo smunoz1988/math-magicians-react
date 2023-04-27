e5e73351c9840b945b59a312466e70e7a080ea23
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-04-26 20:27:34 -0500

    create handler for buttons and insert math logic

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,22 +1,33 @@
 import React, { useState } from 'react';
-//  import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
   const buttons = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-  const [count, setCount] = useState(0);
+  const [result, setResult] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+  const handleKeyPress = (event) => {
+    setResult((prevState) => {
+      const calculator = calculate(prevState, event.target.textContent);
+      return { ...prevState, ...calculator };
+    });
+  };
 
   return (
     <>
       <div className="calculator">
-        <p className="outputResult">{count}</p>
+        <p className="outputResult">
+          {result.total}
+          {result.next}
+          {result.operation}
+        </p>
         <div className="buttonContainer">
           {buttons.map((button) => (
-            <button type="button" className="calcButtons" key={button} onClick={handleClick}>{button}</button>
+            <button type="button" className="calcButtons" key={button} onClick={handleKeyPress}>{button}</button>
           ))}
         </div>
       </div>
