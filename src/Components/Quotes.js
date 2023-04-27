fec27a3e8f470994d992445e194534248443c428
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-04-27 18:36:54 -0500

    Add loading and error states

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -1,21 +1,42 @@
 import { useEffect, useState } from 'react';
 
 const Quotes = () => {
+  const [loading, setLoading] = useState('not loading');
+  const [error, setError] = useState('error');
   const [quotesReceived, setQuote] = useState([]);
 
+  const fetchData = async () => {
+    const category = 'funny';
+    const res = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+      headers: {
+        'X-Api-Key': '6GbkZeAqM5BjI03ACdq/Ig==y1NOsmUqa5LXYEbO',
+      },
+    });
+    setLoading('Loading');
+    const receiveData = await res.json();
+    setQuote(receiveData);
+    setError('not error');
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      const category = 'funny';
-      const res = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
-        headers: {
-          'X-Api-Key': '6GbkZeAqM5BjI03ACdq/Ig==y1NOsmUqa5LXYEbO',
-        },
-      });
-      const json = await res.json();
-      setQuote(json);
-    };
     fetchData();
   }, []);
+
+  if (loading === 'not loading') {
+    return (
+      <p className="quoteAuthor">
+        Loading
+      </p>
+    );
+  }
+
+  if (error === 'error') {
+    return (
+      <p className="quoteAuthor">
+        Error
+      </p>
+    );
+  }
 
   return (
     <div className="quoteContainer">
