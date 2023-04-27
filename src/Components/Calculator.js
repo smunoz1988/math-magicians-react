a06a56479efe58ece9d3f7689ff0fbd00dc43f09
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-04-26 21:03:29 -0500

    fix bug with Division operate

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -2,13 +2,9 @@ import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 const Calculator = () => {
-  const buttons = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 
-  const [result, setResult] = useState({
-    total: 0,
-    next: null,
-    operation: null,
-  });
+  const [result, setResult] = useState({ total: 0, next: null, operation: null });
 
   const handleKeyPress = (clickButton) => {
     setResult((prevState) => {
