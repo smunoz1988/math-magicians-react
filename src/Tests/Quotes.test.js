00406e52c84f86b2cd5c63119e66fe6cd4c14dba
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-05-04 16:04:15 -0500

    update quote test

--- src/Tests/Quotes.test.js
+++ src/Tests/Quotes.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Quotes from '../components/Quotes';
+import Quotes from '../Components/Quotes'
 
 describe('Quotes component', () => {
   it('should match snapshot', () => {
