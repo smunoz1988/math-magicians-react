a6e2140457bb45261ad6caf27d23f35315e85cba
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-05-04 15:51:23 -0500

    include ext on js files for linters

--- src/Tests/Calculator.test.js
+++ src/Tests/Calculator.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Calculator from '../components/Calculator';
+import Calculator from '../components/Calculator.js';
 
 describe('Calculator component', () => {
   it('should match snapshot', () => {
--- src/Tests/Quotes.test.js
+++ src/Tests/Quotes.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import Quotes from '../components/Quotes';
+import Quotes from '../components/Quotes.js';
 
 describe('Quotes component', () => {
   it('should match snapshot', () => {
