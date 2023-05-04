f2232b964f46c9aa8ff7dcd70a1db805467d20c1
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-05-04 15:47:30 -0500

    update calculate test using jest. linters fix

--- src/Tests/Home.test.js
+++ src/Tests/Home.test.js
@@ -11,7 +11,7 @@ describe('it should show content when the user enter the Math home page', () => 
 
   it('when home page is render, the first paragraph should be display', () => {
     const { container } = render(<Home />);
-    const paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat repudiandae dolor similique, nulla quam exercitationem,eveniet rem expedita aliquid excepturi dicta aspernatur. Tempora dolorem consequuntur vero minima maiores dicta at?. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat repudiandae dolor similique, nulla quam exercitationem, eveniet rem expedita aliquid excepturi dicta aspernatur. Tempora dolorem consequuntur vero minima maiores dicta at?'
+    const paragraph = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat repudiandae dolor similique, nulla quam exercitationem,eveniet rem expedita aliquid excepturi dicta aspernatur. Tempora dolorem consequuntur vero minima maiores dicta at?. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat repudiandae dolor similique, nulla quam exercitationem, eveniet rem expedita aliquid excepturi dicta aspernatur. Tempora dolorem consequuntur vero minima maiores dicta at?';
 
     expect(screen.getByText(paragraph)).toBeTruthy();
     expect(container.firstChild).toMatchSnapshot();
--- src/Tests/operate.test.js
+++ src/Tests/operate.test.js
@@ -1,32 +1,32 @@
-import operate from "../logic/operate";
+import operate from '../logic/operate';
 
 describe('test operate', () => {
   test('subtract numbers', () => {
     const test = operate(8, 4, '-');
     expect(test).toBe('4');
-  })
+  });
   test('plus two numbers', () => {
     const test = operate(8, 4, '+');
     expect(test).toBe('12');
-  })
+  });
   test('times two numbers', () => {
     const test = operate(3, 2, 'x');
     expect(test).toBe('6');
-  })
+  });
   test('divide two numbers', () => {
     const test = operate(10, 2, '÷');
     expect(test).toBe('5');
-  })
+  });
   test('divide a number by 0', () => {
     const test = operate(10, 0, '÷');
     expect(test).toBe("Can't divide by 0.");
-  })
+  });
   test('Module a number by 0', () => {
     const test = operate(10, 0, '%');
     expect(test).toBe("Can't find modulo as can't divide by 0.");
-  })
+  });
   test('Module a number by 0', () => {
     const test = operate(7, 5, '%');
     expect(test).toBe('2');
-  })
+  });
 });
