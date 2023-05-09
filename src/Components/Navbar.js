e2dd181b0a68c2194d7e34ba581814bb79d11516
Santiago Muñoz <santiago.munoz.chi@gmail.com>
2023-05-08 21:09:11 -0500

    add hamburguer button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,25 @@
 import { Link, NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
 
-const NavBar = () => (
-  <nav>
-    <h2>Math Magicians</h2>
-    <div className="navItemsContainer">
-      <Link to="/" className="navItems">Home</Link>
-      <NavLink to="/Calculator" className="navItems">Calculator</NavLink>
-      <NavLink to="/Quotes" className="navItems">Quotes</NavLink>
-    </div>
-  </nav>
-);
+const NavBar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <nav>
+      <h2>Math Magicians</h2>
+      <div className="navItemsContainer">
+        <button
+          type="button"
+          className="toggle"
+          onClick={() => setNavbarOpen((prev) => !prev)}
+        >
+          {navbarOpen ? 'close' : 'open'}
+        </button>
+        <Link to="/" className="navItems">Home</Link>
+        <NavLink to="/Calculator" className="navItems">Calculator</NavLink>
+        <NavLink to="/Quotes" className="navItems">Quotes</NavLink>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
